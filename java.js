051e1a1b9c4b48c5faac904aed5593214679fa3b
I_AM_ZENON <z_zenon67@zenon.pamwela>
2026-09-02 02:43:05 +0200

    centered nav

--- java.js
+++ java.js
@@ -8,3 +8,19 @@ function rotateCards () {
 }
 
 setInterval(rotateCards,slideInterval);
+
+ const menuToggle = document.getElementById('mobile-menu');
+    const navMenu = document.querySelector('nav');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    // Close menu automatically when any link is clicked
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', () => {
+            menuToggle.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
